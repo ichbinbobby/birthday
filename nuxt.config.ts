a3be26f9cf70c9ccff319fb67cc760151a2a5f4c
nguyenb <thanh-binh.nguyen@strato.de>
2023-06-21 13:14:46 +0200

    Added opengraph meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,27 @@ export default defineNuxtConfig({
         },
         // <meta name="viewport" content="width=device-width, initial-scale=1">
         { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          property: "og:description",
+          content: "Bobby's birthday page",
+        },
+        {
+          property: "og:image",
+          content:
+            "https://lh3.googleusercontent.com/QTSOFxOaVde6gu-lNjTZ_VrOh7ihpSgRc-hErsBVjQ4Te9tIj4cW4BNWXj2fy1WbDnWYNS3v6pO628xBLC-A74K7ylf0ODNz-jbNhM1OUCgTUvZSN3zP=w960",
+        },
+        {
+          property: "og:title",
+          content: "Bobby's Dirty Thirty",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: "http://geburtstag.ichbinbobby.de/",
+        },
       ],
       title: "Bobby's Dirty Thirty",
     },
